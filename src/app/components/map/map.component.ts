04300e0d100ec65c0edb1f106479e6c11e8e01c8
Andrea Sforza <andrearobertosforza@gmail.com>
2021-06-20 19:44:04 +0200

    styles

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -5,12 +5,14 @@ import { EarthquakesService } from '../../services/earthquakes.service';
 import { Point } from 'geojson';
 
 const getColor = (mag: number) => {
-  return mag >= 8 ? "#C90D1A":
-  mag >= 7 ? "#DA3B18":
-  mag >= 6 ? "#D76A14":
-  mag >= 5 ? "#D49910":
-  mag >= 4 ? "#D1C80C":
-  "#CEF708";
+  return (
+    mag >= 8 ? "#7f0000":
+    mag >= 7 ? "#b71c1c":
+    mag >= 6 ? "#e64a19":
+    mag >= 5 ? "#ff7d47":
+    mag >= 4 ? "#fbc02d":
+               "#fff263"
+  );
 }
 
 @Component({
@@ -49,13 +51,133 @@ export class MapComponent implements AfterViewInit {
 
     this.map.createPane('plates');
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const tileLayerUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileLayerUrl, {	
       maxZoom: 18,
-      minZoom: 3,
+      minZoom: 1,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
     tiles.addTo(this.map); 
+  }
+
+  loadEarthquakes() {
+    const eqCircle = (feature: Feature<Point, any>) => {
+      const { mag } = feature.properties;
+      return {
+        radius: mag * 1.5,
+        weight: 1,
+        color: '#383838',
+        fillColor: getColor(mag),
+        fillOpacity: 0.8
+      }
+    }
+
+    const pointPopup = (feature: Feature<Geometry, any>) => {
+      const { mag, magType, title, time, url } = feature.properties;
+      const [ longitude, latitude, depth ] = Object.values(feature.geometry)[Object.keys(feature.geometry).indexOf('coordinates')]
+      
+      const utcTime = new Date(time).toUTCString();
+
+      return `
+        <h3 class="is-size-6 has-text-weight-semibold">${title}<br>
+        <small class="is-size-7 is-link"><a href=${url} target:"_blank">More info</a></small></h3>
+        <div class="table-container mt-3">
+          <table class="table">
+            <tr>
+              <td class="has-text-weight-semibold">When</td>
+              <td>${utcTime}</td>
+            </tr>
+            <tr>
+              <td class="has-text-weight-semibold">Magnitude</td>
+              <td>${mag} ${magType}</td>
+            </tr>
+            <tr>
+              <td class="has-text-weight-semibold">Latitude</td>
+              <td>${latitude}</td>
+            </tr>
+            <tr>
+              <td class="has-text-weight-semibold">Longitude</td>
+              <td>${longitude}</td>
+            </tr>
+            <tr>
+              <td class="has-text-weight-semibold">Depth</td>
+              <td>${depth} km</td>
+            </tr>
+          </table>
+        </div>
+
+      `
+    }
+    
+    this.earthquakesService.getSignificantEarthquakes()
+    .subscribe((response) => {
+      this.eqMinorLayer = L.geoJSON(response, {
+        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
+        filter: (feature) => feature.properties.mag < 4,
+        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
+      });
+      this.eqCount.push(Object.keys(this.eqMinorLayer._layers).length);
+      this.map.addLayer(this.eqMinorLayer);
+
+      this.eqLightLayer = L.geoJSON(response, {
+        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
+        filter: (feature) => feature.properties.mag >= 4 && feature.properties.mag < 5,
+        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
+      });
+      this.eqCount.push(Object.keys(this.eqLightLayer._layers).length);
+      this.map.addLayer(this.eqLightLayer);
+
+      this.eqModerateLayer = L.geoJSON(response, {
+        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
+        filter: (feature) => feature.properties.mag >= 5 && feature.properties.mag < 6,
+        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
+      });
+      this.eqCount.push(Object.keys(this.eqModerateLayer._layers).length);
+      this.map.addLayer(this.eqModerateLayer);
+
+      this.eqStrongLayer = L.geoJSON(response, {
+        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
+        filter: (feature) => feature.properties.mag >= 6 && feature.properties.mag < 7,
+        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
+      });
+      this.eqCount.push(Object.keys(this.eqStrongLayer._layers).length);
+      this.map.addLayer(this.eqStrongLayer);
+
+      this.eqMajorLayer = L.geoJSON(response, {
+        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
+        filter: (feature) => feature.properties.mag >= 7 && feature.properties.mag < 8,
+        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
+      });
+      this.eqCount.push(Object.keys(this.eqMajorLayer._layers).length);
+      this.map.addLayer(this.eqMajorLayer);
+
+      this.eqGreatLayer = L.geoJSON(response, {
+        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
+        filter: (feature) => feature.properties.mag >= 8,
+        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
+      });
+      this.eqCount.push(Object.keys(this.eqGreatLayer._layers).length);
+      this.map.addLayer(this.eqGreatLayer);
+
+      this.addLegend();
+    })
+  }
+
+  loadPlates() {
+    this.earthquakesService.getTectonicPlates()
+    .subscribe((response) => {
+      this.platesLayer = L.geoJSON(response, {
+        style: {
+          weight: 1,
+          color: '#304ffe'
+        },
+        pane: 'plates'
+      });
+
+      this.map.addLayer(this.platesLayer);
+    })
+
   }
 
   showPlates(show: boolean) {
@@ -100,120 +222,14 @@ export class MapComponent implements AfterViewInit {
     : this.map.removeLayer(this.eqGreatLayer);
   }
 
-  loadEarthquakes() {
-    const eqCircle = (feature: Feature<Point, any>) => {
-      const { mag } = feature.properties;
-      return {
-        radius: mag * 1.5,
-        weight: 1,
-        color: '#383838',
-        fillColor: getColor(mag),
-        fillOpacity: 0.8
-      }
-    }
-
-    const pointPopup = (feature: Feature<Geometry, any>) => {
-      const { mag, magType, title, place, time, url } = feature.properties;
-      const [ longitude, latitude, depth ] = Object.values(feature.geometry)[Object.keys(feature.geometry).indexOf('coordinates')]
-      
-      const timeString = new Date(time);
-      const utcTime = timeString.toUTCString();
-
-      return `
-        <h3>${title}<br>
-        <small><a href=${url} target:"_blank">More info</a></small></h3>
-        <p><strong>When:</strong> ${timeString}<br>
-        <strong>UTC:</strong> ${utcTime}<br>
-        <strong>Magnitude:</strong> ${mag} ${magType}<br>
-        <strong>Latitude:</strong> ${latitude}<br>
-        <strong>Longitude:</strong> ${longitude}<br>
-        <strong>Depth:</strong> ${depth} km<br>
-        <strong>Where:</strong> ${place}<br></p>
-      `
-    }
-    
-    this.earthquakesService.getSignificantEarthquakes()
-    .subscribe((response) => {
-      this.eqMinorLayer = L.geoJSON(response, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
-        filter: (feature) => feature.properties.mag < 4,
-        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
-      });
-      this.eqCount.push(Object.keys(this.eqMinorLayer._layers).length)
-
-      this.eqLightLayer = L.geoJSON(response, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
-        filter: (feature) => feature.properties.mag >= 4 && feature.properties.mag < 5,
-        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
-      });
-      this.eqCount.push(Object.keys(this.eqLightLayer._layers).length)
-
-      this.eqModerateLayer = L.geoJSON(response, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
-        filter: (feature) => feature.properties.mag >= 5 && feature.properties.mag < 6,
-        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
-      });
-      this.eqCount.push(Object.keys(this.eqModerateLayer._layers).length)
-
-      this.eqStrongLayer = L.geoJSON(response, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
-        filter: (feature) => feature.properties.mag >= 6 && feature.properties.mag < 7,
-        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
-      });
-      this.eqCount.push(Object.keys(this.eqStrongLayer._layers).length)
-
-      this.eqMajorLayer = L.geoJSON(response, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
-        filter: (feature) => feature.properties.mag >= 7 && feature.properties.mag < 8,
-        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
-      });
-      this.eqCount.push(Object.keys(this.eqMajorLayer._layers).length)
-
-      this.eqGreatLayer = L.geoJSON(response, {
-        pointToLayer: (feature, latlng) => L.circleMarker(latlng, eqCircle(feature)),
-        filter: (feature) => feature.properties.mag >= 8,
-        onEachFeature: (feature, layer) => layer.bindPopup(pointPopup(feature))
-      });
-      this.eqCount.push(Object.keys(this.eqGreatLayer._layers).length)
-
-
-      this.map.addLayer(this.eqMinorLayer);
-      this.map.addLayer(this.eqLightLayer);
-      this.map.addLayer(this.eqModerateLayer);
-      this.map.addLayer(this.eqStrongLayer);
-      this.map.addLayer(this.eqMajorLayer);
-      this.map.addLayer(this.eqGreatLayer);
-
-      this.addLegend();
-    })
-  }
-
-
-
-  loadPlates() {
-    this.earthquakesService.getTectonicPlates()
-    .subscribe((response) => {
-      this.platesLayer = L.geoJSON(response, {
-        style: {
-          weight: 1,
-          color: '#ff0000'
-        },
-        pane: 'plates'
-      });
-
-      this.map.addLayer(this.platesLayer);
-    })
-
-  }
-
   addLegend() {
     const legend = new L.Control({position: 'bottomleft'});
-    const counter = [...this.eqCount];
+    const counter = this.eqCount;
 
-    legend.onAdd = function (map) {
+    legend.onAdd = function () {
         const div = L.DomUtil.create('div', 'info legend'),
         grades = [2,3,4,5,6,7],
-        labels = ["Minor","Light","Moderate","Strong","Major","Great"];
+        labels = ["Minor | M < 4","Light | M 4-5","Moderate | M 5-6","Strong | M 6-7","Major | M 7-8","Great | M > 8"];
     
         for (let i = 0; i < grades.length; i++) {
           div.innerHTML += `
--- src/app/services/earthquakes.service.ts
+++ src/app/services/earthquakes.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { USGSResponse } from '../USGSResponse';
 import { GeoJSON } from 'geojson';
 
 const httpOptions = {
@@ -13,9 +12,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class EarthquakesService {
-  private significantUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
   private allUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson';
-  private over45Url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson'
   private platesUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json';
 
   constructor(private http:HttpClient) { }
